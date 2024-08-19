75750ca8c36d029501b7670743c116a21650d044
shamshirz <avotre422@gmail.com>
2024-08-19 19:55:18 +0200

    🔥 Remove everything except small text view

--- src/Main2.elm
+++ src/Main2.elm
@@ -29,24 +29,45 @@ main =
 
 type Model
     = Model
-        { selected : List Element
-        , hand : List SelectableElement
-        , view : View
-        , lastCast : Maybe (List Element)
-        , castHistory : List Resolution
+        { castHistory : List Resolution
+        , gameState : GameState
         }
 
 
-type Game
-    = Selection (List SelectableElement) Int Int (List Resolution)
-    | Castable (List SelectableElement) Int Int (List Resolution)
-    | Done (List Resolution)
+type alias Amount =
+    Int
+
+
+type alias TurnsRemaining =
+    Int
+
+
+type SpellEffect
+    = Regen Amount TurnsRemaining
+    | Amplify Amount
+
+
+type Spellbook
+    = Spellbook (AnyDict (List Int) (List Element) Spell)
+
+
+type alias GameData =
+    { planetLife : Int
+    , planetEffects : List SpellEffect
+    , turn : Int
+    , spellbook : Spellbook
+    , elements : List SelectableElement
+    }
+
+
+type GameState
+    = GameState GameData
 
 
 type Resolution
     = Resolution
         { elements : List Element
-        , results : List SpellConfiguration
+        , results : SpellConfiguration
         }
 
 
@@ -58,12 +79,6 @@ type SpellConfiguration
         , effects : List String
         , name : String
         }
-
-
-type View
-    = Discover
-    | Demo
-    | Game Game
 
 
 type SelectableElement
@@ -91,11 +106,15 @@ type Spell
 init : Model
 init =
     Model
-        { selected = []
-        , hand = []
-        , view = Discover
-        , lastCast = Nothing
-        , castHistory = []
+        { castHistory = []
+        , gameState =
+            GameState
+                { planetLife = 0
+                , planetEffects = []
+                , turn = 0
+                , spellbook = Spellbook spellbook
+                , elements = [ SelectableElement Fire False, SelectableElement Water False, SelectableElement Earth False ]
+                }
         }
 
 
@@ -104,139 +123,29 @@ init =
 
 
 type Msg
-    = Add Element
-    | Backspace
-    | Remove Int
-    | Draw
-    | ToggleElement Int
-    | ToggleView
-    | Cast
-    | Play
-    | GameCast
-    | GameToggleElement Int
+    = GameToggleElement Int
 
 
 update : Msg -> Model -> Model
 update msg (Model model) =
+    let
+        (GameState gameData) =
+            model.gameState
+
+        toggleElementAtIndex : Int -> List SelectableElement -> List SelectableElement
+        toggleElementAtIndex index =
+            List.indexedMap
+                (\idx (SelectableElement el isSelected) ->
+                    if idx == index then
+                        SelectableElement el (not isSelected)
+
+                    else
+                        SelectableElement el isSelected
+                )
+    in
     case msg of
-        Add element ->
-            Model { model | selected = element :: model.selected }
-
-        Backspace ->
-            Model { model | selected = model.selected |> List.tail |> Maybe.withDefault [] }
-
-        Remove index ->
-            Model { model | selected = List.Extra.removeAt index model.selected }
-
-        Draw ->
-            Model { model | hand = List.append model.hand [ SelectableElement Fire False, SelectableElement Water False, SelectableElement Earth False ] }
-
-        ToggleElement index ->
-            let
-                newHand =
-                    List.Extra.updateAt index (\(SelectableElement element selected) -> SelectableElement element (not selected)) model.hand
-            in
-            Model { model | hand = newHand }
-
-        ToggleView ->
-            case model.view of
-                Discover ->
-                    Model { model | view = Demo }
-
-                Demo ->
-                    Model { model | view = Discover }
-
-                Game _ ->
-                    Model model
-
-        Cast ->
-            let
-                ( selectedElements, unselectedElements ) =
-                    List.partition (\(SelectableElement _ s) -> s) model.hand
-
-                resolution =
-                    resolveElements (List.map (\(SelectableElement element _) -> element) selectedElements)
-            in
-            Model
-                { model
-                    | hand = unselectedElements
-                    , lastCast = Just (List.map (\(SelectableElement element _) -> element) selectedElements)
-                    , castHistory = resolution :: model.castHistory
-                }
-
-        Play ->
-            Model { model | view = Game (Selection [ SelectableElement Fire False, SelectableElement Water False, SelectableElement Earth False, SelectableElement Wind False ] 10 10 []) }
-
-        GameCast ->
-            let
-                newGameState =
-                    case model.view of
-                        Game game ->
-                            case game of
-                                Done x ->
-                                    Done x
-
-                                Selection hand player enemy castHistory ->
-                                    Selection hand player enemy castHistory
-
-                                Castable hand player enemy castHistory ->
-                                    let
-                                        ( selectedElements, unselectedElements ) =
-                                            List.partition (\(SelectableElement _ s) -> s) hand
-
-                                        resolution =
-                                            resolveElements (List.map (\(SelectableElement element _) -> element) selectedElements)
-
-                                        damage =
-                                            List.foldl (\(Resolution data) acc -> List.foldl (\(SpellConfiguration instance) iAcc -> iAcc + instance.life) acc data.results) 0 castHistory
-                                    in
-                                    if enemy - damage <= 0 then
-                                        Done (resolution :: castHistory)
-
-                                    else
-                                        Selection (addDraw unselectedElements) player (enemy - damage) (resolution :: castHistory)
-
-                        _ ->
-                            Selection [] 0 0 []
-            in
-            Model { model | view = Game newGameState }
-
         GameToggleElement index ->
-            let
-                newGameState =
-                    case model.view of
-                        Game game ->
-                            case game of
-                                Done x ->
-                                    Done x
-
-                                Selection hand player enemy castHistory ->
-                                    Castable
-                                        (List.Extra.updateAt index (\(SelectableElement element selected) -> SelectableElement element (not selected)) hand)
-                                        player
-                                        enemy
-                                        castHistory
-
-                                Castable hand player enemy castHistory ->
-                                    let
-                                        newHand =
-                                            List.Extra.updateAt index (\(SelectableElement element selected) -> SelectableElement element (not selected)) hand
-                                    in
-                                    if List.any (\(SelectableElement _ s) -> s) newHand then
-                                        Castable newHand player enemy castHistory
-
-                                    else
-                                        Selection newHand player enemy castHistory
-
-                        _ ->
-                            Selection [] 0 0 []
-            in
-            Model { model | view = Game newGameState }
-
-
-addDraw : List SelectableElement -> List SelectableElement
-addDraw hand =
-    List.append hand [ SelectableElement Fire False, SelectableElement Water False, SelectableElement Earth False ]
+            Model { model | gameState = GameState { gameData | elements = toggleElementAtIndex index gameData.elements } }
 
 
 displaySpellbook : Html Msg
@@ -254,6 +163,15 @@ displaySpellbook =
                 |> Dict.Any.toList
                 |> List.map (\( _, spell ) -> displayGrouping (spellConfiguration spell))
             )
+        ]
+
+
+displayGrouping : SpellConfiguration -> Html Msg
+displayGrouping (SpellConfiguration { name, elements }) =
+    div [ style "background-color" "#CBC3E3", style "margin" "5px", style "border-radius" "5px", style "padding" "5px" ]
+        [ span [ style "text-align" "center" ] [ text name ]
+        , br [] []
+        , span [] <| List.indexedMap (\index e -> dot (GameToggleElement index) e) elements
         ]
 
 
@@ -395,212 +313,56 @@ spellbook =
 
 
 view : Model -> Html Msg
-view (Model model) =
-    case model.view of
-        Discover ->
-            div [ style "background-color" "#f28d85", style "border-radius" "5px" ] [ button [ onClick ToggleView ] [ text "Toggle" ], button [ onClick Play ] [ text "Play!" ], viewDiscover model.selected ]
-
-        Demo ->
-            div [ style "background-color" "#bff2aa", style "border-radius" "5px" ] [ button [ onClick ToggleView ] [ text "Toggle" ], button [ onClick Play ] [ text "Play!" ], viewDemo model.hand model.castHistory ]
-
-        Game game ->
-            div [ style "background-color" "#F5F5F5", style "border-radius" "5px" ] [ viewGame game ]
+view (Model { gameState }) =
+    case gameState of
+        GameState gameData ->
+            div [] [ viewState gameData, viewActions gameData ]
 
 
-viewGame : Game -> Html Msg
-viewGame game =
-    case game of
-        Selection hand player enemy castHistory ->
-            div []
-                [ viewSelect hand player enemy castHistory
-                , div [ id "history" ]
-                    [ h2 [] [ text "History" ]
-                    , div [] (List.map viewResolution castHistory)
-                    ]
-                ]
-
-        Castable hand player enemy castHistory ->
-            div []
-                [ viewSelect hand player enemy castHistory
-                , button [ onClick GameCast ] [ text "Cast" ]
-                , div [ id "history" ]
-                    [ h2 [] [ text "History" ]
-                    , div [] (List.map viewResolution castHistory)
-                    ]
-                ]
-
-        Done castHistory ->
-            viewGameOver castHistory
+viewActions : GameData -> Html Msg
+viewActions gameData =
+    div [] (List.indexedMap (\idx se -> viewSelectableElement (GameToggleElement idx) se) gameData.elements)
 
 
-viewSelect : List SelectableElement -> Int -> Int -> List Resolution -> Html Msg
-viewSelect hand player enemy castHistory =
-    div []
-        [ viewGameStats player enemy castHistory
-        , div [ style "display" "flex", style "width" "100%" ]
-            [ div [ id "hand", style "display" "flex", style "flex-direction" "column", style "width" "50%", style "align-items" "center" ]
-                [ h2 [] [ text "Hand" ]
-                , div [ style "display" "flex", style "flex-direction" "row", style "align-items" "flex-end", style "height" "50px" ]
-                    (List.indexedMap (\index se -> viewSelectableElement (GameToggleElement index) se) hand)
-                ]
-            , div [ id "on-deck", style "display" "flex", style "flex-direction" "column", style "width" "50%", style "align-items" "center" ]
-                [ h2 [] [ text "On Deck" ]
-                , div [ style "display" "flex", style "flex-direction" "row", style "justify-content" "center" ]
-                    (parseAsGrouping (hand |> List.filter (\(SelectableElement _ s) -> s) |> List.map (\(SelectableElement element _) -> element)))
-                ]
-            ]
-        ]
-
-
-viewGameOver : List Resolution -> Html Msg
-viewGameOver castHistory =
-    div []
-        [ viewGameStats 10 0 castHistory
-        , div [ id "history" ]
-            [ h2 [] [ text "History" ]
-            , div [] (List.map viewResolution castHistory)
-            ]
-        ]
-
-
-viewDemo : List SelectableElement -> List Resolution -> Html Msg
-viewDemo hand castHistory =
-    div []
-        [ div [ style "display" "flex", style "justify-content" "space-between" ]
-            [ div []
-                [ text <| String.fromInt <| length <| hand
-                , button [ onClick Draw ] [ text "Draw" ]
-                ]
-            , div [ id "hand", style "display" "flex" ]
-                [ h2 [] [ text "Hand now" ]
-                , div [ style "display" "flex", style "flex-direction" "row" ] (List.indexedMap (\index se -> viewSelectableElement (ToggleElement index) se) hand)
-                ]
-            , div [ id "on-deck", style "display" "flex" ]
-                [ h2 [] [ text "On Deck" ]
-                , div [ style "display" "flex", style "flex-direction" "row" ]
-                    (parseAsGrouping (hand |> List.filter (\(SelectableElement _ s) -> s) |> List.map (\(SelectableElement element _) -> element)))
-                ]
-            ]
-        , div [ style "display" "flex" ]
-            [ h2 [] [ text "Resolution" ]
-            , text "tmp"
-            , button [ onClick Cast ] [ text "Cast" ]
-            ]
-        , div [ id "history" ]
-            [ h2 [] [ text "History" ]
-            , div [ style "display" "flex" ]
-                [ div []
-                    [ text "Turns: "
-                    , text <| String.fromInt <| length castHistory
-                    , br [] []
-                    , text "Life: "
-                    , text <| String.fromInt <| List.foldl (\(Resolution data) acc -> List.foldl (\(SpellConfiguration instance) iAcc -> iAcc + instance.life) acc data.results) 0 castHistory
-                    ]
-                , div [] (List.map viewResolution castHistory)
-                ]
-            ]
-        ]
-
-
-viewGameStats : Int -> Int -> List Resolution -> Html Msg
-viewGameStats player enemy castHistory =
+viewState : GameData -> Html Msg
+viewState gameData =
     let
-        turns =
-            length castHistory
+        spellEffectToString : SpellEffect -> String
+        spellEffectToString spellEffect =
+            case spellEffect of
+                Regen amount turns ->
+                    "Regen:" ++ String.fromInt amount ++ String.fromInt turns
 
-        damage =
-            List.foldl (\(Resolution data) acc -> List.foldl (\(SpellConfiguration instance) iAcc -> iAcc + instance.life) acc data.results) 0 castHistory
+                Amplify amount ->
+                    "Amplify:" ++ String.fromInt amount
+
+        joinSpellEffects : List SpellEffect -> String
+        joinSpellEffects se =
+            se |> List.map spellEffectToString |> List.foldl (++) ""
+
+        selectableElementToString : SelectableElement -> String
+        selectableElementToString (SelectableElement element isSelected) =
+            if isSelected then
+                " ("
+                    ++ elementToString element
+                    ++ ") "
+
+            else
+                " "
+                    ++ elementToString element
+                    ++ " "
     in
-    div [ id "game-info", style "display" "flex", style "justify-content" "space-between", style "flex-direction" "row" ]
-        [ h2 [] [ text "Game Deets" ]
-        , span [] [ text "Player: ", text <| String.fromInt player ]
-        , span [] [ text "Enemy: ", text <| String.fromInt enemy ]
-        , span [] [ text "Turns: ", text <| String.fromInt <| turns ]
-        , span [] [ text "Damage: ", text <| String.fromInt <| damage ]
+    div []
+        [ div [] [ text "Planet Life: ", text (String.fromInt gameData.planetLife) ]
+        , div [] [ text "Planet Effects: ", text (joinSpellEffects gameData.planetEffects) ]
+        , div [] [ text "Turn: ", text (String.fromInt gameData.turn) ]
+        , div [] [ text "Elements: ", text (gameData.elements |> List.map selectableElementToString |> List.foldl (++) "") ]
         ]
-
-
-viewDiscover : List Element -> Html Msg
-viewDiscover selected =
-    div [ style "display" "flex", style "align-content" "flex-start" ]
-        [ div [ id "spellbook" ]
-            [ h2 [] [ text "Spellbook" ]
-            , div [ style "display" "flex", style "flex-direction" "column" ] [ displaySpellbook ]
-            ]
-        , div [ id "selection", style "flex-grow" "2", style "margin-left" "10%" ]
-            [ h2 [] [ text "Add" ]
-            , div [ style "display" "flex" ]
-                [ dot (Add Earth) Earth
-                , dot (Add Fire) Fire
-                , dot (Add Water) Water
-                , dot (Add Wind) Wind
-                ]
-            , br [] []
-            , text "Selected"
-            , div [] [ button [ onClick Backspace ] [ text "Backspace" ] ]
-            , div [] (List.indexedMap (\index e -> dot (Remove index) e) selected)
-            , div [ id "results" ]
-                [ h2 [] [ text "Results" ]
-                , div [ style "display" "flex" ] (parseAsGrouping selected)
-                , br [] []
-                ]
-            ]
-        ]
-
-
-displayGrouping : SpellConfiguration -> Html Msg
-displayGrouping (SpellConfiguration { name, elements }) =
-    div [ style "background-color" "#CBC3E3", style "margin" "5px", style "border-radius" "5px", style "padding" "5px" ]
-        [ span [ style "text-align" "center" ] [ text name ]
-        , br [] []
-        , span [] <| List.indexedMap (\index e -> dot (Remove index) e) elements
-        ]
-
-
-resolveElements : List Element -> Resolution
-resolveElements elements =
-    Resolution { elements = elements, results = foldElements elements }
-
-
-{-| Find an entry in the spellbook, or treat each element individually
--}
-foldElements : List Element -> List SpellConfiguration
-foldElements elements =
-    case castSpell elements of
-        Just spell ->
-            [ spellConfiguration spell ]
-
-        Nothing ->
-            elements
-                |> List.map (\e -> castSpell [ e ])
-                |> List.filterMap identity
-                |> List.map spellConfiguration
-
-
-parseAsGrouping : List Element -> List (Html Msg)
-parseAsGrouping elements =
-    elements
-        |> foldElements
-        |> List.map displayGrouping
 
 
 castSpell : List Element -> Maybe Spell
 castSpell elements =
     Dict.Any.get elements spellbook
-
-
-viewResolution : Resolution -> Html Msg
-viewResolution (Resolution data) =
-    let
-        viewSpellConfiguration : SpellConfiguration -> Html Msg
-        viewSpellConfiguration (SpellConfiguration instance) =
-            div [ style "display" "flex" ]
-                [ displayGrouping (SpellConfiguration instance)
-                , div [] [ text "Damage: ", text (String.fromInt instance.life) ]
-                , div [] (text " Effects: " :: List.map (\e -> text e) instance.effects)
-                ]
-    in
-    div [] (List.map viewSpellConfiguration data.results)
 
 
 elementToString : Element -> String
